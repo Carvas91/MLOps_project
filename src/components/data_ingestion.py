a567ceb218ac40598cf54210f199edee50a96bbf
carvas91 <carvas91@hotmail.com>
2024-09-20 10:02:06 -0400

    commit

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,5 +6,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
-
-
+@dataclass
+class DataIngetionConfig:
+    train_data_path: str=os.path.join('artifacts', 'train_csv')
+    test_data_path: str = os.path.join('artifacts', 'test_csv')
+    raw_data_path: str = os.path.join('artifacts', 'raw_csv')
